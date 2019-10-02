08d9d69987541c9b7375d881af50b67f15ba3c61
rafaelreisramos <rrramos@gmail.com>
2019-10-02 11:24:41 -0300

    added cors to allow api access

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const routes = require('./routes');
 
 const app = express();
 
-//mongoose.connect('put your mongo_url here', {
-mongoose.connect('put you mongo_url here', {
+mongoose.connect('put your mongo_url here', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
